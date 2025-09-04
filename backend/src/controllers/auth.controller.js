a615a919144e5e09f9182a1ea493bc2c1d767aa3
Suhanchuppa <suhandsa22@gmail.com>
2025-09-04 20:11:54 +0530

    optimised code

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const login = async function(req, res){
+    res.send("hello login");
+}
+
+export const logout = async function(req, res){
+    res.send("hello logout");
+}
+
+export const signup = async function(req, res){
+    res.send("hello signup");
+}
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,17 +1,11 @@
 import express from 'express';
-
+import { login, signup,logout } from '../controllers/auth.controller.js';
 const router = express.Router();
 
-router.get('/signup', (req, res) => {
-    res.send('Hello signup');
-});
+router.post('/signup',signup);
 
-router.get('/login', (req, res) => {
-    res.send('Hello login');
-});
+router.post('/login', login);
 
-router.get('/logout', (req, res) => {
-    res.send('Hello logout');
-});
+router.post('/logout', logout);
 
 export default router;
